6303ffb9e01a22a3e3ccc7927ad4ed7f61a9ea43
Mike Gretton <mikegretton@gmail.com>
2020-05-05 22:19:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // write your code below!
 function happyHolidays() { return "Happy holidays!"; }
-function happyHolidaysTo(name) { 
+function happyHolidaysTo(name) {
   return `Happy holidays, ${name}!`;
 }
 function happyCustomHolidayTo(holiday, name) {
